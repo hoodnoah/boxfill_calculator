26ef48779bde2b355646e4839e590771304f135e
Noah Hood <hood.noah@icloud.com>
2024-01-26 11:45:55 -0500

    simplify local state in TerminalBlockInput component

--- src/components/TerminalBlockInput.vue
+++ src/components/TerminalBlockInput.vue
@@ -1,34 +1,32 @@
 <script setup lang="ts">
 import { Option } from '@/lib/Option'
 import { AWGConductor } from '@/lib/BoxFill'
-import { ref } from 'vue'
-
-const emit = defineEmits(['update:modelValue'])
 
 const props = defineProps<{ largestAWG: AWGConductor }>()
 const terminalBlocksModel = defineModel<Option.Option<AWGConductor[]>>({ required: true })
-
-const terminalBlocksDisplayValue = ref(Option.getOrDefault(terminalBlocksModel.value, []))
+const emit = defineEmits(['update:modelValue'])
 
 function addNewTerminalBlock() {
   const newTerminalBlock = props.largestAWG
-  terminalBlocksDisplayValue.value.push(newTerminalBlock)
-  emit('update:modelValue', Option.Some(terminalBlocksDisplayValue.value))
+  const updatedBlocks = Option.getOrDefault(terminalBlocksModel.value, [])
+  updatedBlocks.push(newTerminalBlock)
+  emit('update:modelValue', Option.Some(updatedBlocks))
 }
 
-function removeTerminalBlock(id: number): void {
-  terminalBlocksDisplayValue.value.splice(id, 1)
-  if (terminalBlocksDisplayValue.value.length == 0) {
+function removeTerminalBlock(index: number): void {
+  const updatedBlocks = Option.getOrDefault(terminalBlocksModel.value, [])
+  updatedBlocks.splice(index, 1)
+  if (updatedBlocks.length === 0) {
     emit('update:modelValue', Option.None())
   } else {
-    emit('update:modelValue', Option.Some(terminalBlocksDisplayValue.value))
+    emit('update:modelValue', Option.Some(updatedBlocks))
   }
 }
 </script>
 
 <template>
   <button name="addTerminalBlock" @click="addNewTerminalBlock">+ add terminal block</button>
-  <div v-for="(block, index) in terminalBlocksDisplayValue" :key="index">
+  <div v-for="(block, index) in Option.getOrDefault(terminalBlocksModel, [])" :key="index">
     {{ block + ' AWG' }}
     <button @click="removeTerminalBlock(index)">- remove terminal block</button>
   </div>
