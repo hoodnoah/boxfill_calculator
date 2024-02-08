decbe45384afe60056222bac9dc51773915099d2
Noah Hood <86262760+hoodnoah@users.noreply.github.com>
2024-02-08 16:33:34 +0000

    Release 0.0.1 a2 (#8)
    * Add unit test on pull request (#1)
    
    * add unit test on pull request
    
    * rename workflows file to reflect action
    
    * fix name of test jobs
    
    * Implement minimum working example of box fill calculator in compliance w/ 2023 NFPA NEC (#3)
    
    * add functionality for calculating box fill with only basic conductors
    
    * add support for the following additional fill types: clamp, support fitting, device/equipment, equipment grounding, and terminal block
    
    * use inheritance to generalize getAllowance across conductors, devices, grounding wires, etc.
    
    * streamline getXFill functions to be of uniform argument type
    
    * simplify Result return from getBoxFill function
    
    * implement state-reactive component for inputting conductors
    
    * update unitSystemToggle to support state-reactive toggling
    
    * add Options utilities, refactor unit tests, etc. to use them; implement toggle for usage of internal clamps
    
    * make clamps update internal state when largest AWG changes
    
    * update state to factor common largest conductor out of individual state elements
    
    * update components to reflect simplified state
    
    * update SupportFittingsInput.vue to reflect simplified state
    
    * add functionality for adding/removing devices
    
    * add functionality to account for grounding conductors
    
    * add functionality for adding terminal blocks
    
    * simplify local state in TerminalBlockInput component
    
    * remove console logs
    
    * start implementing cypress e2e tests
    
    * implement defaults testing w/ cypress
    
    * fix bug where incorrect device would be deleted
    
    * add AWG selection option for a terminal block
    
    * complete minimum working example
    
    * add changelog
    
    * update tests, lib to reflect usage of Option types, and updated understanding of box fill calculations
    
    * remove outdated component tests
    
    * add github workflow for e2e testing on pull request, fix outdated tests (#4)
    
    * add github pages deployment action (#5)
    
    * Style (#7)
    
    * add custom favicon
    
    * checkpoint
    
    * delete unused icons
    
    * remove default vue styling
    
    * update styling for BoxFillDisplay
    
    * style unit conversion button, h2, and select
    
    * update styling from default html
    
    * update end-to-end tests to reflect new element names/hierarchy
    
    * update changelog
    
    * update changelog to reflect release number
    
    * remove edge from testing due to inconsistent behavior on CI servers
    
    * update test name to reflect matrix testing strategy

--- src/components/AWGInput.vue
+++ src/components/AWGInput.vue
@@ -2,6 +2,7 @@
 import { AWGConductor } from '@/lib/BoxFill'
 
 const emit = defineEmits(['update:modelValue'])
+const props = defineProps<{ header?: string }>()
 
 const awgSelected = defineModel({ type: Number as () => AWGConductor, required: true })
 
@@ -9,12 +10,21 @@ const awgSelected = defineModel({ type: Number as () => AWGConductor, required: 
 const awgOptions = Object.values(AWGConductor).filter((x) => !isNaN(Number(x)))
 
 function updateAWGModel() {
-  emit('update:modelValue', awgSelected.value)
+    emit('update:modelValue', awgSelected.value)
 }
 </script>
 
 <template>
-  <select v-model="awgSelected" @input="updateAWGModel" name="awgInput">
-    <option v-for="awg in awgOptions" :key="awg" :value="awg">{{ 'AWG ' + awg }}</option>
-  </select>
+    <div class="awg-input-wrapper">
+        <h2 v-if="props.header !== undefined && props.header !== ''">largest conductor in box:</h2>
+        <select v-model="awgSelected" @input="updateAWGModel" name="awgInput">
+            <option v-for="awg in awgOptions" :key="awg" :value="awg">{{ 'AWG ' + awg }}</option>
+        </select>
+    </div>
 </template>
+
+<style scoped>
+select {
+    width: 100%;
+}
+</style>
--- src/components/TerminalBlockInput.vue
+++ src/components/TerminalBlockInput.vue
@@ -4,39 +4,79 @@ import { AWGConductor } from '@/lib/BoxFill'
 import AWGInput from '@/components/AWGInput.vue'
 
 // Used for defaults
-const props = defineProps<{ largestAWG: AWGConductor }>()
+const props = defineProps<{ largestAWG: AWGConductor, header?: string }>()
 
 const terminalBlocksModel = defineModel<Option.Option<AWGConductor[]>>({ required: true })
 const emit = defineEmits(['update:modelValue'])
 
 function addNewTerminalBlock() {
-  const newTerminalBlock = props.largestAWG
-  const updatedBlocks = Option.getOrDefault(terminalBlocksModel.value, [])
-  updatedBlocks.push(newTerminalBlock)
-  emit('update:modelValue', Option.Some(updatedBlocks))
+    const newTerminalBlock = props.largestAWG
+    const updatedBlocks = Option.getOrDefault(terminalBlocksModel.value, [])
+    updatedBlocks.push(newTerminalBlock)
+    emit('update:modelValue', Option.Some(updatedBlocks))
 }
 
 function removeTerminalBlock(index: number): void {
-  const updatedBlocks = Option.getOrDefault(terminalBlocksModel.value, [])
-  updatedBlocks.splice(index, 1)
-  if (updatedBlocks.length === 0) {
-    emit('update:modelValue', Option.None())
-  } else {
-    emit('update:modelValue', Option.Some(updatedBlocks))
-  }
+    const updatedBlocks = Option.getOrDefault(terminalBlocksModel.value, [])
+    updatedBlocks.splice(index, 1)
+    if (updatedBlocks.length === 0) {
+        emit('update:modelValue', Option.None())
+    } else {
+        emit('update:modelValue', Option.Some(updatedBlocks))
+    }
 }
 </script>
 
 <template>
-  <button name="addTerminalBlock" @click="addNewTerminalBlock">+ add terminal block</button>
-  <div
-    class="terminalBlocksDisplay"
-    v-for="(block, index) in Option.getOrDefault(terminalBlocksModel, [])"
-    :key="index"
-  >
-    <AWGInput v-model="Option.getOrDefault(terminalBlocksModel, [])[index]" />
-    <button name="deleteTerminalBlock" @click="removeTerminalBlock(index)">
-      - remove terminal block
-    </button>
-  </div>
+    <div class="terminal-blocks-header">
+        <h2 v-if="props.header !== undefined && props.header != ''">{{ props.header }}</h2>
+        <button class="add-item-button" name="addTerminalBlock" @click="addNewTerminalBlock">+</button>
+    </div>
+    <div class="terminal-blocks-list">
+        <div class="terminalBlocksDisplay" v-for="(block, index) in Option.getOrDefault(terminalBlocksModel, [])"
+            :key="index">
+            <AWGInput class="terminal-block-awg-input" v-model="Option.getOrDefault(terminalBlocksModel, [])[index]" />
+            <button class="delete-item-button" name="deleteTerminalBlock" @click="removeTerminalBlock(index)">
+                X
+            </button>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+@import url('@/assets/main.css');
+
+.terminal-blocks-header {
+    display: flex;
+    flex-flow: row nowrap;
+    gap: 1em;
+    align-items: baseline;
+}
+
+.terminalBlocksDisplay {
+    display: flex;
+    flex-flow: row nowrap;
+    width: 100%;
+    justify-content: flex-end;
+    align-items: baseline;
+    gap: 1em;
+}
+
+.terminal-blocks-list {
+    display: flex;
+    flex-flow: column nowrap;
+    gap: .5em;
+}
+
+.terminal-block-awg-input {
+    flex-grow: 1;
+}
+
+.add-item-button {
+    background-color: var(--color-button-green);
+}
+
+.delete-item-button {
+    background-color: var(--color-button-red);
+}
+</style>
